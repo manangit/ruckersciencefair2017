8f45820a0e4c5c8ab1e94ce2b6250352fa8c0f0e
Manan Sharma <manan.sharma.21@outlook.com>
2017-02-19 14:45:43 -0600

    Update one-switch-light.ino

--- one-switch-light.ino
+++ one-switch-light.ino
@@ -1,99 +1,65 @@
-/*
-  My Science Fair Project
+/*                                
+  My Science Fair Project   
   The one switch Smart House
 */
-#include <Servo.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+#include <TM1637Display.h>  // 4-digit Diplay library
 
-Servo doorOpener;
-LiquidCrystal_I2C lcd(0x3F, 2,1,0,4,5,6,7,3,POSITIVE);
-int porchLight = 2;
-int coachLights = 4;
-int backyardLight = 8;
-int lightSwitch = 12;
-int servoMotor = 6;
-int doorOpenerSwitch = 9;
-bool switchState = LOW;
-bool lastSwitchState = LOW;
-bool isPorchLightOn = false;
-bool isCoachLightOn = false;
-bool isBackyardLightOn = false;
-bool isDoorOpen = false;
+const int CLK = 6;          // Set the CLK pin connection to the display
+const int DIO = 7;          // Set the DIO pin connection to the display
 
-int timer = 0;
+TM1637Display display(CLK, DIO);  // set up the 4-Digit Display.
 
-void setup()
+int porchLight = 2;         // Prorch light should be conected to pin 2 on the arduino
+int coachLights = 4;        // Coach lights should be conected to pin 4 on the arduino
+int backyardLight = 8;      // Backyard light should be conected to pin 8 on the arduino
+int switchPin = 12;         // Switch should be conected to pin 12 on the arduino
+bool isPorchLightOn = false; // To track if the light is on or off
+bool areCoachLightsOn = false; // To track if the light is on or off
+bool isBackyardLightOn = false; // To track if the light is on or off
+
+int timer = 0;             // 
+
+void setup()               // It is used to initialize variables, pin modes, etc. The setup function will only run once.
 {
-  pinMode(porchLight, OUTPUT);
-  pinMode(coachLights, OUTPUT);
-  pinMode(backyardLight, OUTPUT);
-  pinMode(lightSwitch, INPUT);
-  
-  doorOpener.attach(servoMotor);
-  
-  lcd.begin(16,2);
-  lcd.clear();
-  lcd.print("start");
-  Serial.begin(9600);
+  pinMode(porchLight, OUTPUT); // Telling the arduino that the Light pin is an output
+  pinMode(coachLights, OUTPUT); // Telling the arduino that the Light pin is an output
+  pinMode(backyardLight, OUTPUT); // Telling the arduino that the Light pin is an output
+  pinMode(switchPin, INPUT); // Telling the arduino that Switch pin is an input 
+
+  display.setBrightness(10); // Set brightness of LED display to 100%
+
+  Serial.begin(9600);       //For debugging on serial port
 }
 
-
-boolean debounce(boolean last)
+void loop()                 // This function runs forever until it is on 
 {
-  boolean current = digitalRead(doorOpenerSwitch);
-  if (last != current)
-  {
-    delay(50);
-    current = digitalRead(doorOpenerSwitch);
-  }
-  return current;
-}
-
-
-void loop()
-{
-//  doorOpener.write (0);
-//  switchState = ;
-  while (debounce(lastSwitchState) == HIGH)
-  {
-    if(isDoorOpen){
-      doorOpener.write(0);
-      isDoorOpen = false;
-    }
-    else{
-      doorOpener.write(150);
-      isDoorOpen = true;
-    }
-    Serial.println(isDoorOpen);
-  }
-  lastSwitchState = !lastSwitchState;
-  
-  while (digitalRead(lightSwitch) == HIGH)
+  while (digitalRead(switchPin) == HIGH) //
   {
     delay (100);
     timer = timer + 100;
-    lcd.print(timer/100);
-    lcd.clear();
+    display.showNumberDec(timer);
+    Serial.println(timer);
   }
+
   if (timer > 100 && timer <= 1000)
   {
     isPorchLightOn = !isPorchLightOn;
     digitalWrite(porchLight, isPorchLightOn);
     timer = 0;
   }
+
   if ( timer > 1000 && timer <= 3000)
   {
-    isCoachLightOn = !isCoachLightOn;
-    digitalWrite(coachLights, isCoachLightOn);
+    areCoachLightsOn = !areCoachLightsOn;
+    digitalWrite(coachLights, areCoachLightsOn);
     timer = 0;
   }
+
   if (timer > 3000)
   {
     isBackyardLightOn = !isBackyardLightOn;
     digitalWrite(backyardLight, isBackyardLightOn);
     timer = 0;
   }
-//  lcd.clear();
-  
+  display.showNumberDec(0);
 }
