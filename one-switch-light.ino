058f8f0f53e971b3eba8ecb77ef93e07d59e590b
Manan Sharma <manan.sharma.21@outlook.com>
2017-02-19 14:44:56 -0600

    Update one-switch-light.ino

--- one-switch-light.ino
+++ one-switch-light.ino
@@ -2,16 +2,19 @@
   My Science Fair Project
   The one switch Smart House
 */
+#include <Servo.h>
+Servo doorOpener;
+
 
 int porchLight = 2;
 int coachLights = 4;
 int backyardLight = 8;
-int switchPin = 12;
-boolean switchState = LOW;
-boolean lastSwitchState = LOW;
-boolean isPorchLightOn = false;
-boolean isCoachLightOn = false;
-boolean isBackyardLightOn = false;
+int lightSwitch = 12;
+bool switchState = LOW;
+bool lastSwitchState = LOW;
+bool isPorchLightOn = false;
+bool isCoachLightOn = false;
+bool isBackyardLightOn = false;
 
 int timer = 0;
 
@@ -20,32 +23,28 @@ void setup()
   pinMode(porchLight, OUTPUT);
   pinMode(coachLights, OUTPUT);
   pinMode(backyardLight, OUTPUT);
-  pinMode(switchPin, INPUT);
-
+  pinMode(lightSwitch, INPUT);
 }
 
 void loop()
 {
-  while (digitalRead(switchPin) == HIGH)
+  while (digitalRead(lightSwitch) == HIGH)
   {
     delay (100);
     timer = timer + 100;
   }
-
   if (timer > 100 && timer <= 1000)
   {
     isPorchLightOn = !isPorchLightOn;
     digitalWrite(porchLight, isPorchLightOn);
     timer = 0;
   }
-
   if ( timer > 1000 && timer <= 3000)
   {
     isCoachLightOn = !isCoachLightOn;
     digitalWrite(coachLights, isCoachLightOn);
     timer = 0;
   }
-
   if (timer > 3000)
   {
     isBackyardLightOn = !isBackyardLightOn;
